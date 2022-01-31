18ec3aba34310d3779e17c7f408f4e850b1f3799
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-31 09:24:22 -0800

    Create get_browser_history.ps1

---
+++ windows/get_browser_history.ps1
@@ -0,0 +1,30 @@
+$Param = if ($args[0]) { $args[0] | ConvertFrom-Json }
+$Output = foreach ($User in (Get-WmiObject Win32_UserProfile | Where-Object {
+$_.localpath -notmatch 'Windows' }).localpath) {
+    foreach ($Path in @('AppData\Local\Google\Chrome\User Data\Default\History',
+    'AppData\Local\Microsoft\Edge\User Data\Default\History')) {
+        $History = Join-Path $User $Path
+        if (Test-Path $History) {
+            $Domain = '(htt(p|ps))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+            Get-Content $History | Select-String -AllMatches $Domain |
+            ForEach-Object { ($_.Matches).Value } | Sort-Object -Unique | ForEach-Object {
+                if ($_ -match $Search) {
+                    [PSCustomObject] @{
+                        Username = $User | Split-Path -Leaf
+                        Browser  = if ($History -match 'Chrome') { 'Chrome' } else { 'Edge' }
+                        Domain   = $_
+                    }
+                }
+            }
+        }
+    }
+}
+if ($Param.Filter) {
+    $Output = $Output | Where-Object { $_.Domain -match $Param.Filter }
+}
+if ($Output -and $Param.Log -eq $true) {
+    $Rtr = Join-Path $env:SystemRoot 'system32\drivers\CrowdStrike\Rtr'
+    if ((Test-Path $Rtr) -eq $false) { New-Item $Rtr -ItemType Directory }
+    $Output | ForEach-Object { $_ | ConvertTo-Json -Compress >> "$Rtr\get_browser_history.json" }
+}
+$Output | ForEach-Object { $_ | ConvertTo-Json -Compress }
