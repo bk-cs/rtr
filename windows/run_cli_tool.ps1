48a7099e1119ec5879fb33928e94854a7b9fb025
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-03 16:11:44 -0800

    Created

---
+++ windows/run_cli_tool.ps1
@@ -0,0 +1,51 @@
+function Confirm-FilePath ([string] $String) {
+    $String = $String -replace '\\\\','\'
+    if ($String -match '^\\Device') {
+        $Def = @'
+[DllImport("kernel32.dll", SetLastError = true)]
+public static extern uint QueryDosDevice(
+    string lpDeviceName,
+    System.Text.StringBuilder lpTargetPath,
+    uint ucchMax);
+'@
+        $StrBld = New-Object System.Text.StringBuilder(65536)
+        $K32 = Add-Type -MemberDefinition $Def -Name Kernel32 -Namespace Win32 -PassThru
+        foreach ($Vol in (Get-WmiObject Win32_Volume | Where-Object { $_.DriveLetter })) {
+            [void] $K32::QueryDosDevice($Vol.DriveLetter,$StrBld,65536)
+            $Ntp = [regex]::Escape($StrBld.ToString())
+            $String -replace $Ntp, $Vol.DriveLetter
+        }
+    } else {
+        $String
+    }
+}
+$Param = if ($args[0]) { $args[0] | ConvertFrom-Json }
+$Path = Confirm-FilePath $Param.Path
+if ((Test-Path $Path) -eq $false) {
+    throw "Cannot find path '$Path' because it does not exist."
+} elseif ((Test-Path $Path -PathType Leaf) -eq $false) {
+    throw "'Path' must be a file."
+}
+$Json = "run_cli_tool_$((Get-Date).ToFileTimeUtc()).json"
+$Rtr = Join-Path $env:SystemRoot 'system32\drivers\CrowdStrike\Rtr'
+if ((Test-Path $Rtr) -eq $false) { New-Item $Rtr -ItemType Directory }
+$Start = @{
+    FilePath               = $Path
+    RedirectStandardOutput = "$Rtr\$Json"
+    PassThru               = $true
+}
+if ($Param.ArgumentList) {
+    $Start['ArgumentList'] = $Param.ArgumentList
+}
+Start-Process @Start | ForEach-Object {
+    $_.PSObject.Properties.Add((New-Object PSNoteProperty('Output',($Start.RedirectStandardOutput))))
+    if ($Param.Delete -eq $true) {
+        $Wait = @{
+            FilePath     = 'powershell.exe'
+            ArgumentList = "-Command &{ Wait-Process -Id $($_.Id); Remove-Item -Path $Path }"
+            PassThru     = $true
+        }
+        [void] (Start-Process @Wait)
+    }
+    $_ | Select-Object Id, ProcessName, Output | ConvertTo-Json -Compress
+}
