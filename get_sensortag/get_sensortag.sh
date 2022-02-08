7090b1281cf2bbdd3b3f2ce7501bbe3437af10b0
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-08 11:35:31 -0800

    Update get_sensortag.sh

--- get_sensortag/get_sensortag.sh
+++ get_sensortag/get_sensortag.sh
@@ -1,2 +1,14 @@
-tags=$(/opt/CrowdStrike/falconctl -g --tags | sed 's/^Sensor grouping tags are not set.//; s/^tags=//; s/.$//')
-echo "{\"SensorTag\": \"${tags}\"}"
+#!/bin/bash
+write() {
+  local log="/opt/CrowdStrike/RTR/get_sensortag_$(date +%s).json"
+  local json='{"SensorTag":"%s"}'
+  if [ "$2" = '{"Log":true}' ]; then
+    if [ ! -d "/opt/CrowdStrike/RTR" ]; then
+      mkdir "/opt/CrowdStrike/RTR"
+    fi
+    printf "$json" "$1" >> "$log"
+  fi
+  printf "$json" "$1"
+}
+tag=$(/opt/CrowdStrike/falconctl -g --tags | sed 's/^Sensor grouping tags are not set.//; s/^tags=//; s/.$//')
+write "$tag" "$1"
