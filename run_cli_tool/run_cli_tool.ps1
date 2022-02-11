1eabff7032184ee43b3bb0318e9bb8ae0ec6a782
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-11 13:07:05 -0800

    Added error message
    Added error when 'Path' or 'File' is not supplied.

--- run_cli_tool/run_cli_tool.ps1
+++ run_cli_tool/run_cli_tool.ps1
@@ -1,28 +1,32 @@
 function Confirm-FilePath ([string] $String) {
-    if ($String -match 'HarddiskVolume\d+\\') {
-        $Def = @'
+    if (![string]::IsNullOrEmpty($String)) {
+        if ($String -match 'HarddiskVolume\d+\\') {
+            $Def = @'
 [DllImport("kernel32.dll", SetLastError = true)]
 public static extern uint QueryDosDevice(
     string lpDeviceName,
     System.Text.StringBuilder lpTargetPath,
     uint ucchMax);
 '@
-        $StrBld = New-Object System.Text.StringBuilder(65536)
-        $K32 = Add-Type -MemberDefinition $Def -Name Kernel32 -Namespace Win32 -PassThru
-        foreach ($Vol in (Get-WmiObject Win32_Volume | Where-Object { $_.DriveLetter })) {
-            [void] $K32::QueryDosDevice($Vol.DriveLetter,$StrBld,65536)
-            $Ntp = [regex]::Escape($StrBld.ToString())
-            $String | Where-Object { $_ -match $Ntp } | ForEach-Object {
-                $_ -replace $Ntp, $Vol.DriveLetter
+            $StrBld = New-Object System.Text.StringBuilder(65536)
+            $K32 = Add-Type -MemberDefinition $Def -Name Kernel32 -Namespace Win32 -PassThru
+            foreach ($Vol in (Get-WmiObject Win32_Volume | Where-Object { $_.DriveLetter })) {
+                [void] $K32::QueryDosDevice($Vol.DriveLetter,$StrBld,65536)
+                $Ntp = [regex]::Escape($StrBld.ToString())
+                $String | Where-Object { $_ -match $Ntp } | ForEach-Object {
+                    $_ -replace $Ntp, $Vol.DriveLetter
+                }
             }
+        } else {
+            $String
         }
-    } else {
-        $String
     }
 }
 $Param = if ($args[0]) { $args[0] | ConvertFrom-Json }
 $Path = Confirm-FilePath $Param.Path
-if ((Test-Path $Path) -eq $false) {
+if (!$Path) {
+    throw "Missing required parameter 'Path'."
+} elseif ((Test-Path $Path) -eq $false) {
     throw "Cannot find path '$Path' because it does not exist."
 } elseif ((Test-Path $Path -PathType Leaf) -eq $false) {
     throw "'Path' must be a file."
