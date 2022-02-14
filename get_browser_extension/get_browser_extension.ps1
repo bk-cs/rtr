2ff86c892bf9f4c2058b2f075ae42c79677516d6
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-14 11:52:51 -0800

    Added output properties
    Added 'ManifestVersion', 'ContentSecurityPolicy', 'OfflineEnabled' and 'Permissions'

--- get_browser_extension/get_browser_extension.ps1
+++ get_browser_extension/get_browser_extension.ps1
@@ -20,25 +20,26 @@ $_.localpath -notmatch 'Windows' }).localpath) {
                         Get-Content $Json | ConvertFrom-Json | ForEach-Object {
                             [PSCustomObject] @{
                                 Username = $User | Split-Path -Leaf
-                                Browser  = if ($ExtPath -match 'Chrome') { 'Chrome' } else { 'Edge' }
-                                Name     = if ($_.Name -notlike '__MSG*') { $_.Name } else {
-                                    $Id = ($_.Name -replace '__MSG_','').Trim('_')
+                                Browser = if ($ExtPath -match 'Chrome') { 'Chrome' } else { 'Edge' }
+                                Name = if ($_.name -notlike '__MSG*') { $_.name } else {
+                                    $Id = ($_.name -replace '__MSG_','').Trim('_')
                                     @('_locales\en_US','_locales\en').foreach{
-                                        $Msg = Join-Path -Path (
-                                            Join-Path -Path $Item.Fullname -ChildPath $_) -ChildPath messages.json
+                                        $Msg = Join-Path (Join-Path $Item.Fullname $_) messages.json
                                         if (Test-Path -Path $Msg) {
                                             $App = Get-Content $Msg | ConvertFrom-Json
                                             (@('appName','extName','extensionName','app_name',
                                             'application_title',$Id).foreach{
-                                                if ($App.$_.message) {
-                                                    $App.$_.message
-                                                }
+                                                if ($App.$_.message) {  $App.$_.message }
                                             }) | Select-Object -First 1
                                         }
                                     }
                                 }
-                                Id       = $Folder.Name
-                                Version  = $_.Version
+                                Id = $Folder.Name
+                                Version = $_.version
+                                ManifestVersion = $_.manifest_version
+                                ContentSecurityPolicy = $_.content_security_policy
+                                OfflineEnabled = if ($_.offline_enabled) { $_.offline_enabled } else { $false }
+                                Permissions = $_.permissions
                             } | ForEach-Object { if ($Param.Filter) {
                                 $_ | Where-Object { $_.Extension -match $Param.Filter }} else { $_ }
                             }
