b970ddb21d7cd89d057256ab29eb9052df7fa48f
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-18 13:34:58 -0800

    Updated functions, added aliases
    Updated 'Write-Output' to 'output'. The function now creates the entire Humio submission body when logged to the local disk. This way the created Json files will be associated with the original host if they're ingested using another system.

--- disable_service/disable_service.ps1
+++ disable_service/disable_service.ps1
@@ -1,24 +1,38 @@
-function Write-Output ([object] $Object, [object] $Param, [string] $Json) {
-    if ($Object -and $Param.Log -eq $true) {
+function output ([object] $Obj, [object] $Param, [string] $Json) {
+    if ($Obj -and $Param.Log -eq $true) {
         $Rtr = Join-Path $env:SystemRoot 'system32\drivers\CrowdStrike\Rtr'
-        if ((Test-Path $Rtr) -eq $false) { New-Item $Rtr -ItemType Directory }
-        $Object | ForEach-Object { $_ | ConvertTo-Json -Compress >> "$Rtr\$Json" }
+        if ((Test-Path $Rtr -PathType Container) -eq $false) { ni $Rtr -ItemType Directory }
+        $O = @{ tags = @{ json = $Json; script = $Json -replace '_\d+\.json$','.ps1';
+            host = [System.Net.Dns]::GetHostName() }}
+        $R = reg query ('HKEY_LOCAL_MACHINE\SYSTEM\CrowdStrike\{9b03c1d9-3138-44ed-9fae-d9f4c034b88d}\{16e0423f-' +
+            '7058-48c9-a204-725362b67639}\Default') 2>$null
+        if ($R) {
+            $O.tags['cid'] = (($R -match 'CU ') -split 'REG_BINARY')[-1].Trim().ToLower()
+            $O.tags['aid'] = (($R -match 'AG ') -split 'REG_BINARY')[-1].Trim().ToLower()
+        }
+        $Evt = $Obj | % {
+            $Att = @{}
+            $_.PSObject.Properties | % { $Att[$_.Name]=$_.Value }
+            ,@{ timestamp = Get-Date -Format o; attributes = $Att }
+        }
+        if (($Evt | measure).Count -eq 1) {
+            $O['events'] = @($Evt)
+            $O | ConvertTo-Json -Depth 8 -Compress >> (Join-Path $Rtr $Json)
+        } elseif (($Evt | measure).Count -gt 1) {
+            for ($i = 0; $i -lt ($Evt | measure).Count; $i += 200) {
+                $C = $O.Clone()
+                $C['events'] = $Evt[$i..($i + 199)]
+                $C | ConvertTo-Json -Depth 8 -Compress >> (Join-Path $Rtr $Json)
+            }
+        }
     }
-    $Object | ForEach-Object { $_ | ConvertTo-Json -Compress }
+    $Obj | ConvertTo-Json -Compress
 }
 $Param = if ($args[0]) { $args[0] | ConvertFrom-Json }
-if (-not $Param.Name) {
-    throw "Missing required parameter 'Name'."
-}
-$Service = Get-Service | Where-Object { $_.Name -eq $Param.Name }
-if (-not $Service) {
-    throw "No results for service '$($Param.Name)'."
-}
-if ($Service.StartType) {
-    $Service | Set-Service -StartupType Disabled
-}
-if ($Service.Status -ne 'Stopped') {
-    $Service | Set-Service -Status Stopped
-}
-$Output = Get-Service -Name $Param.Name | Select-Object Name, Status, StartType
-Write-Output $Output $Param "$Rtr\disable_service_$((Get-Date).ToFileTimeUtc()).json"
+if (-not $Param.Name) { throw "Missing required parameter 'Name'." }
+$Service = gsv | ? { $_.Name -eq $Param.Name }
+if (-not $Service) { throw "No results for service '$($Param.Name)'." }
+if ($Service.StartType) { $Service | Set-Service -StartupType Disabled }
+if ($Service.Status -ne 'Stopped') { $Service | Set-Service -Status Stopped }
+$Out = gsv -Name $Param.Name | select Name, Status, StartType
+output $Out $Param "disable_service_$((Get-Date).ToFileTimeUtc()).json"
