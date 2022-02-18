7da01afeb690a1cb3723b6cf682c84f3eb10de17
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-18 14:33:05 -0800

    Updated log output
    Updated output to match humio event format and removed 'RedirectStandardOutput'.

--- get_temp_file/get_temp_file.ps1
+++ get_temp_file/get_temp_file.ps1
@@ -1,8 +1,8 @@
 [scriptblock] $Script = {
-    param( [string] $UserDir )
-    function Get-UniqueHash ([object] $Obj, [string] $Str) {
+    param( [string] $Json, [string] $UserDir )
+    function hash ([object] $Obj, [string] $Str) {
         foreach ($I in $Obj) {
-            $E = ($Obj | Where-Object { $_.$Str -eq $I.$Str } | Select-Object -Unique).Sha256
+            $E = ($Obj | ? { $_.$Str -eq $I.$Str } | select -Unique).Sha256
             $H = if ($E) { $E } else { try { (Get-FileHash $I.$Str -EA 0).Hash.ToLower() } catch { $null }}
             $I.PSObject.Properties.Add((New-Object PSNoteProperty('Sha256',$H)))
         }
@@ -11,38 +11,62 @@
     [array] $Dir = if ($UserDir) {
         Join-Path $UserDir 'Appdata\Local\Temp'
     } else {
-        (Get-WmiObject win32_userprofile | Where-Object { $_.SID -match '^S-1-5-21' }).LocalPath |
-            ForEach-Object { Join-Path $_ 'Appdata\Local\Temp' }
+        (gwmi win32_userprofile | ? { $_.SID -match '^S-1-5-21' }).LocalPath | % {
+            Join-Path $_ 'Appdata\Local\Temp' }
         Join-Path $env:SystemRoot 'Temp'
     }
-    $Output = $Dir.foreach{
+    $Obj = $Dir.foreach{
         $Select = @('FullName','Length','CreationTime','LastWriteTime','LastAccessTime')
-        Get-ChildItem $_ -Recurse -File -EA 0 | Select-Object $Select | ForEach-Object {
-            $_.PSObject.Properties | ForEach-Object { if ($_.Value -is [datetime]) { $_.Value = try {
-                $_.Value.ToFileTimeUtc() } catch { $_.Value }}}
+        gci $_ -Recurse -File -EA 0 | select $Select | % {
+            $_.PSObject.Properties | % {
+                if ($_.Value -is [datetime]) { $_.Value = try { $_.Value.ToFileTimeUtc() } catch { $_.Value }}
+            }
             $_
         }
     }
-    Get-UniqueHash $Output FullName | ForEach-Object { $_ | ConvertTo-Json -Compress }
+    $Obj = hash $Obj FullName
+    $O = @{ tags = @{ json = $Json; script = $Json -replace '_\d+\.json$','.ps1';
+        host = [System.Net.Dns]::GetHostName() }}
+    $R = reg query ('HKEY_LOCAL_MACHINE\SYSTEM\CrowdStrike\{9b03c1d9-3138-44ed-9fae-d9f4c034b88d}\{16e0423f-7058' +
+        '-48c9-a204-725362b67639}\Default') 2>$null
+    if ($R) {
+        $O.tags['cid'] = (($R -match 'CU ') -split 'REG_BINARY')[-1].Trim().ToLower()
+        $O.tags['aid'] = (($R -match 'AG ') -split 'REG_BINARY')[-1].Trim().ToLower()
+    }
+    $Evt = @($Obj).foreach{
+        $Att = @{}
+        $_.PSObject.Properties | % { $Att[$_.Name]=$_.Value }
+        ,@{ timestamp = Get-Date -Format o; attributes = $Att }
+    }
+    if (($Evt | measure).Count -eq 1) {
+        $O['events'] = @($Evt)
+        $O | ConvertTo-Json -Depth 8 -Compress >> $Json
+    } elseif (($Evt | measure).Count -gt 1) {
+        for ($i = 0; $i -lt ($Evt | measure).Count; $i += 200) {
+            $C = $O.Clone()
+            $C['events'] = $Evt[$i..($i + 199)]
+            $C | ConvertTo-Json -Depth 8 -Compress >> $Json
+        }
+    }
 }
 $Param = if ($args[0]) { $args[0] | ConvertFrom-Json }
 $Rtr = Join-Path $env:SystemRoot 'system32\drivers\CrowdStrike\Rtr'
-if ((Test-Path $Rtr) -eq $false) { New-Item $Rtr -ItemType Directory }
+if ((Test-Path $Rtr) -eq $false) { ni $Rtr -ItemType Directory }
 if ($Param.Username) {
-    $UserDir = (Get-WmiObject win32_userprofile | Where-Object {
+    $UserDir = (gwmi win32_userprofile | ? {
         $_.LocalPath -match "$([regex]::Escape($Param.Username))$" }).LocalPath
     if (-not $UserDir) {
         throw "No username found matching '$($Param.Username)'."
     }
 }
+$Json = Join-Path $Rtr "get_temp_file_$((Get-Date).ToFileTimeUtc()).json"
 $Start = @{
     FilePath               = 'powershell.exe'
-    ArgumentList           = "-Command &{$Script}"
-    RedirectStandardOutput = "$Rtr\get_temp_file_$((Get-Date).ToFileTimeUtc()).json"
+    ArgumentList           = "-Command &{$Script} '$Json'"
     PassThru               = $true
 }
 if ($UserDir) { $Start.ArgumentList += " '$UserDir'" }
-Start-Process @Start | Select-Object Id, ProcessName | ForEach-Object {
-    $_.PSObject.Properties.Add((New-Object PSNoteProperty('Json',$Start.RedirectStandardOutput)))
+start @Start | select Id, ProcessName | % {
+    $_.PSObject.Properties.Add((New-Object PSNoteProperty('Json',$Json)))
     $_ | ConvertTo-Json -Compress
 }
