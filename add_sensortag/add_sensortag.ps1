d442318bd3c36de4c766ef03da66b988591baa13
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-18 13:33:38 -0800

    Updated functions, added aliases
    Updated 'Write-Output' to 'output'. The function now creates the entire Humio submission body when logged to the local disk. This way the created Json files will be associated with the original host if they're ingested using another system.

--- add_sensortag/add_sensortag.ps1
+++ add_sensortag/add_sensortag.ps1
@@ -1,24 +1,45 @@
-function Write-Output ([object] $Object, [object] $Param, [string] $Json) {
-    if ($Object -and $Param.Log -eq $true) {
+function output ([object] $Obj, [object] $Param, [string] $Json) {
+    if ($Obj -and $Param.Log -eq $true) {
         $Rtr = Join-Path $env:SystemRoot 'system32\drivers\CrowdStrike\Rtr'
-        if ((Test-Path $Rtr) -eq $false) { New-Item $Rtr -ItemType Directory }
-        $Object | ForEach-Object { $_ | ConvertTo-Json -Compress >> "$Rtr\$Json" }
+        if ((Test-Path $Rtr -PathType Container) -eq $false) { ni $Rtr -ItemType Directory }
+        $O = @{ tags = @{ json = $Json; script = $Json -replace '_\d+\.json$','.ps1';
+            host = [System.Net.Dns]::GetHostName() }}
+        $R = reg query ('HKEY_LOCAL_MACHINE\SYSTEM\CrowdStrike\{9b03c1d9-3138-44ed-9fae-d9f4c034b88d}\{16e0423f-' +
+            '7058-48c9-a204-725362b67639}\Default') 2>$null
+        if ($R) {
+            $O.tags['cid'] = (($R -match 'CU ') -split 'REG_BINARY')[-1].Trim().ToLower()
+            $O.tags['aid'] = (($R -match 'AG ') -split 'REG_BINARY')[-1].Trim().ToLower()
+        }
+        $Evt = $Obj | % {
+            $Att = @{}
+            $_.PSObject.Properties | % { $Att[$_.Name]=$_.Value }
+            ,@{ timestamp = Get-Date -Format o; attributes = $Att }
+        }
+        if (($Evt | measure).Count -eq 1) {
+            $O['events'] = @($Evt)
+            $O | ConvertTo-Json -Depth 8 -Compress >> (Join-Path $Rtr $Json)
+        } elseif (($Evt | measure).Count -gt 1) {
+            for ($i = 0; $i -lt ($Evt | measure).Count; $i += 200) {
+                $C = $O.Clone()
+                $C['events'] = $Evt[$i..($i + 199)]
+                $C | ConvertTo-Json -Depth 8 -Compress >> (Join-Path $Rtr $Json)
+            }
+        }
     }
-    $Object | ForEach-Object { $_ | ConvertTo-Json -Compress }
+    $Obj | ConvertTo-Json -Compress
 }
 $Param = if ($args[0]) { $args[0] | ConvertFrom-Json }
 $Key = 'HKEY_LOCAL_MACHINE\SYSTEM\CrowdStrike\{9b03c1d9-3138-44ed-9fae-d9f4c034b88d}\{16e0423f-7058-48c9-a204-72' +
     '5362b67639}\Default'
 if ($Param.SensorTag) {
-    $Tag = (reg query $Key) -match 'GroupingTags'
+    $Tag = (reg query $Key 2>$null) -match 'GroupingTags'
     $Val = if ($Tag) {
-        (($Tag -split 'REG_SZ')[-1].Trim().Split(',') + @($Param.SensorTag) | Select-Object -Unique) -join ','
+        (($Tag -split 'REG_SZ')[-1].Trim().Split(',') + @($Param.SensorTag) | select -Unique) -join ','
     } else {
         $Param.SensorTag -join ','
     }
     [void] (reg add $Key /v GroupingTags /d $Val /f)
 }
-$Output = [PSCustomObject] @{
-    SensorTag = "$((((reg query $Key) -match 'GroupingTags') -split 'REG_SZ')[-1].Trim())"
-}
-Write-Output $Output $Param "add_sensortag_$((Get-Date).ToFileTimeUtc()).json"
+$Out = [PSCustomObject] @{ SensorTag = "$(
+    (((reg query $Key 2>$null) -match 'GroupingTags') -split 'REG_SZ')[-1].Trim())" }
+output $Out $Param "add_sensortag_$((Get-Date).ToFileTimeUtc()).json"
--- set_local_password/set_local_password.ps1
+++ set_local_password/set_local_password.ps1
@@ -1,13 +1,35 @@
-function Write-Output ([object] $Object, [object] $Param, [string] $Json) {
-    if ($Object -and $Param.Log -eq $true) {
+function output ([object] $Obj, [object] $Param, [string] $Json) {
+    if ($Obj -and $Param.Log -eq $true) {
         $Rtr = Join-Path $env:SystemRoot 'system32\drivers\CrowdStrike\Rtr'
-        if ((Test-Path $Rtr) -eq $false) { New-Item $Rtr -ItemType Directory }
-        $Object | ForEach-Object { $_ | ConvertTo-Json -Compress >> "$Rtr\$Json" }
+        if ((Test-Path $Rtr -PathType Container) -eq $false) { ni $Rtr -ItemType Directory }
+        $O = @{ tags = @{ json = $Json; script = $Json -replace '_\d+\.json$','.ps1';
+            host = [System.Net.Dns]::GetHostName() }}
+        $R = reg query ('HKEY_LOCAL_MACHINE\SYSTEM\CrowdStrike\{9b03c1d9-3138-44ed-9fae-d9f4c034b88d}\{16e0423f-' +
+            '7058-48c9-a204-725362b67639}\Default') 2>$null
+        if ($R) {
+            $O.tags['cid'] = (($R -match 'CU ') -split 'REG_BINARY')[-1].Trim().ToLower()
+            $O.tags['aid'] = (($R -match 'AG ') -split 'REG_BINARY')[-1].Trim().ToLower()
+        }
+        $Evt = $Obj | % {
+            $Att = @{}
+            $_.PSObject.Properties | % { $Att[$_.Name]=$_.Value }
+            ,@{ timestamp = Get-Date -Format o; attributes = $Att }
+        }
+        if (($Evt | measure).Count -eq 1) {
+            $O['events'] = @($Evt)
+            $O | ConvertTo-Json -Depth 8 -Compress >> (Join-Path $Rtr $Json)
+        } elseif (($Evt | measure).Count -gt 1) {
+            for ($i = 0; $i -lt ($Evt | measure).Count; $i += 200) {
+                $C = $O.Clone()
+                $C['events'] = $Evt[$i..($i + 199)]
+                $C | ConvertTo-Json -Depth 8 -Compress >> (Join-Path $Rtr $Json)
+            }
+        }
     }
-    $Object | ForEach-Object { $_ | ConvertTo-Json -Compress }
+    $Obj | ConvertTo-Json -Compress
 }
 $Param = if ($args[0]) { $args[0] | ConvertFrom-Json }
-$Output = if ($PSVersionTable.PSVersion.ToString() -gt 5) {
+$Out = if ($PSVersionTable.PSVersion.ToString() -gt 5) {
     try {
         Set-LocalUser -Name $Param.Username -Password ($Param.Password |
             ConvertTo-SecureString -AsPlainText -Force)
@@ -23,17 +45,10 @@ $Output = if ($PSVersionTable.PSVersion.ToString() -gt 5) {
         throw $_
     }
 }
-$Session = Get-Process -IncludeUserName -EA 0 | Where-Object { $_.SessionId -ne 0 -and $_.UserName -match
-    $Param.Username } | Select-Object SessionId, UserName | Sort-Object -Unique
+$Session = ps -IncludeUserName -EA 0 | ? { $_.SessionId -ne 0 -and $_.UserName -match $Param.Username } |
+    select SessionId, UserName | sort -Unique
 $Active = if ($Session.SessionId) {
-    if ($Param.ForceLogoff -eq $true) {
-        logoff $Session.SessionId
-        $false
-    } else {
-        $true
-    }
-} else {
-    $false
-}
-$Output.PSObject.Properties.Add((New-Object PSNoteProperty('ActiveSession',$Active)))
-Write-Output $Output $Param "set_local_password_$((Get-Date).ToFileTimeUtc()).json"
+    if ($Param.ForceLogoff -eq $true) { logoff $Session.SessionId; $false } else { $true }
+} else { $false }
+$Out.PSObject.Properties.Add((New-Object PSNoteProperty('ActiveSession',$Active)))
+output $Out $Param "set_local_password_$((Get-Date).ToFileTimeUtc()).json"
