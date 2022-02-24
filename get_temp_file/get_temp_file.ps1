d2996dc2c764016a5371c41b22251817c5c0cf21
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-24 13:34:31 -0800

    Changed Humio payload
    Changed Humio payload to not use tags and instead add 'host', 'cid' and 'aid' as fields to increase search performance.

--- get_temp_file/get_temp_file.ps1
+++ get_temp_file/get_temp_file.ps1
@@ -10,7 +10,7 @@
     }
     function output ([object] $Obj, [string] $Script, [string] $Cloud, [string] $Token) {
         if (-not $Obj) {
-            $Obj = @{ error = "no_results" }
+            $Obj = @{ error = 'no_results' }
         }
         if ($Cloud -and $Token) {
             $Iwr = @{ Uri = @($Cloud, 'api/v1/ingest/humio-structured/') -join $null; Method = 'post';
@@ -19,41 +19,23 @@
         $Rtr = Join-Path $env:SystemRoot 'system32\drivers\CrowdStrike\Rtr'
         if ((Test-Path $Rtr -PathType Container) -eq $false) { ni $Rtr -ItemType Directory }
         $Json = $Script -replace '\.ps1', ('_' + [string] (Get-Date).ToFileTimeUtc() + '.json')
-        $O = @{ tags = @{ script = $Script; host = [System.Net.Dns]::GetHostName() }}
+        $A = @{ script = $Script; host = [System.Net.Dns]::GetHostName() }
         $R = reg query ('HKEY_LOCAL_MACHINE\SYSTEM\CrowdStrike\{9b03c1d9-3138-44ed-9fae-d9f4c034b88d}\{16e0423f-' +
             '7058-48c9-a204-725362b67639}\Default') 2>$null
         if ($R) {
-            $O.tags['cid'] = (($R -match 'CU ') -split 'REG_BINARY')[-1].Trim().ToLower()
-            $O.tags['aid'] = (($R -match 'AG ') -split 'REG_BINARY')[-1].Trim().ToLower()
+            $A['cid'] = (($R -match 'CU ') -split 'REG_BINARY')[-1].Trim().ToLower()
+            $A['aid'] = (($R -match 'AG ') -split 'REG_BINARY')[-1].Trim().ToLower()
         }
         $E = @($Obj).foreach{
-            $Att = @{}; $_.PSObject.Properties | % { $Att[$_.Name]=$_.Value }
-            ,@{ timestamp = Get-Date -Format o; attributes = $Att }
+            $C = $A.Clone()
+            $_.PSObject.Properties | % { $C[$_.Name]=$_.Value }
+            ,@{ timestamp = Get-Date -Format o; attributes = $C }
         }
-        if (($E | measure).Count -eq 1) {
-            $O['events'] = @($E)
-            if ($Iwr) {
-                try {
-                    iwr @Iwr -Body (ConvertTo-Json @($O) -Depth 8 -Compress) -UseBasicParsing
-                } catch {
-                    ConvertTo-Json @($O) -Depth 8 -Compress >> (Join-Path $Rtr $Json)
-                }
-            } else {
-                ConvertTo-Json @($O) -Depth 8 -Compress >> (Join-Path $Rtr $Json)
-            }
-        } elseif (($E | measure).Count -gt 1) {
-            for ($i = 0; $i -lt ($E | measure).Count; $i += 200) {
-                $C = $O.Clone(); $C['events'] = $E[$i..($i + 199)]
-                if ($Iwr) {
-                    try {
-                        iwr @Iwr -Body (ConvertTo-Json @($C) -Depth 8 -Compress) -UseBasicParsing
-                    } catch {
-                        ConvertTo-Json @($C) -Depth 8 -Compress >> (Join-Path $Rtr $Json)
-                    }
-                } else {
-                    ConvertTo-Json @($C) -Depth 8 -Compress >> (Join-Path $Rtr $Json)
-                }
-            }
+        for ($i = 0; $i -lt ($E | measure).Count; $i += 200) {
+            $B = @{events=@($E[$i..($i + 199)])}
+            $Req = try { iwr @Iwr -Body (ConvertTo-Json @($B) -Depth 8 -Compress) -UseBasicParsing
+            } catch { $null }
+            if ($Req.StatusCode -ne 200) { ConvertTo-Json @($B) -Depth 8 -Compress >> (Join-Path $Rtr $Json) }
         }
     }
     [array] $Dir = if ($Username) {
