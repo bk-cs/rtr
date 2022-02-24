b5ad2b61f950484e1689dcb09a9e742afceb8478
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-24 13:12:06 -0800

    Changed Humio payload
    Changed Humio payload to not use tags and instead add 'host', 'cid' and 'aid' as fields to increase search performance.

--- get_application/get_application.ps1
+++ get_application/get_application.ps1
@@ -24,29 +24,22 @@ function output ([object] $Obj, [object] $Param, [string] $Script) {
         $Json = $Script -replace '\.ps1', "_$((Get-Date).ToFileTimeUtc()).json"
         $Iwr = @{ Uri = @($Param.Cloud, 'api/v1/ingest/humio-structured/') -join $null; Method = 'post';
             Headers = @{ Authorization = @('Bearer', $Param.Token) -join ' '; ContentType = 'application/json' }}
-        $O = @{ tags = @{ script = $Script; host = [System.Net.Dns]::GetHostName() }}
+        $A = @{ script = $Script; host = [System.Net.Dns]::GetHostName() }
         $R = reg query ('HKEY_LOCAL_MACHINE\SYSTEM\CrowdStrike\{9b03c1d9-3138-44ed-9fae-d9f4c034b88d}\{16e0423f-' +
             '7058-48c9-a204-725362b67639}\Default') 2>$null
         if ($R) {
-            $O.tags['cid'] = (($R -match 'CU ') -split 'REG_BINARY')[-1].Trim().ToLower()
-            $O.tags['aid'] = (($R -match 'AG ') -split 'REG_BINARY')[-1].Trim().ToLower()
+            $A['cid'] = (($R -match 'CU ') -split 'REG_BINARY')[-1].Trim().ToLower()
+            $A['aid'] = (($R -match 'AG ') -split 'REG_BINARY')[-1].Trim().ToLower()
         }
         $E = @($Obj).foreach{
-            $Att = @{}; $_.PSObject.Properties | % { $Att[$_.Name]=$_.Value }
-            ,@{ timestamp = Get-Date -Format o; attributes = $Att }}
-        if (($E | measure).Count -eq 1) {
-            $O['events'] = @($E)
-            $Req = try { iwr @Iwr -Body (ConvertTo-Json @($O) -Depth 8 -Compress) -UseBasicParsing } catch {}
+            $_.PSObject.Properties | % { $A[$_.Name]=$_.Value }
+            ,@{ timestamp = Get-Date -Format o; attributes = $A }
+        }
+        for ($i = 0; $i -lt ($E | measure).Count; $i += 200) {
+            $B = @{events=@($E[$i..($i + 199)])}
+            $Req = try { iwr @Iwr -Body (ConvertTo-Json @($B) -Depth 8 -Compress) -UseBasicParsing } catch {}
             if ($Req.StatusCode -ne 200) {
-                ConvertTo-Json @($O) -Depth 8 -Compress >> (Join-Path $Rtr $Json)
-            }
-        } elseif (($E | measure).Count -gt 1) {
-            for ($i = 0; $i -lt ($E | measure).Count; $i += 200) {
-                $C = $O.Clone(); $C['events'] = $E[$i..($i + 199)]
-                $Req = try { iwr @Iwr -Body (ConvertTo-Json @($C) -Depth 8 -Compress) -UseBasicParsing } catch {}
-                if ($Req.StatusCode -ne 200) {
-                    ConvertTo-Json @($C) -Depth 8 -Compress >> (Join-Path $Rtr $Json)
-                }
+                ConvertTo-Json @($B) -Depth 8 -Compress >> (Join-Path $Rtr $Json)
             }
         }
     }
