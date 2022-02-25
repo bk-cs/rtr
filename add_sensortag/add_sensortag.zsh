46cf5e409c3934a54579539a2175e3399de2f5e8
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-02-24 17:02:02 -0800

    Temporarily removed
    Removing bash/zsh scripts while they're re-written to fit the new "stream to Humio" format

--- add_sensortag/add_sensortag.zsh
+++
@@ -1,7 +0,0 @@
-IFS=,
-tag=($(/Applications/Falcon.app/Contents/Resources/falconctl grouping-tags get | sed "s/^No grouping tags set//; s/^Grouping tags: //"))
-tag+=($@)
-uniq=$(echo "${tag[@]}" | tr " " "\n" | sort -u | tr "\n" "," | sed "s/,$//")
-/Applications/Falcon.app/Contents/Resources/falconctl grouping-tags clear &> /dev/null
-/Applications/Falcon.app/Contents/Resources/falconctl grouping-tags set "$uniq" &> /dev/null
-/Applications/Falcon.app/Contents/Resources/falconctl grouping-tags get | sed "s/^No grouping tags set//; s/^Grouping tags: //"
